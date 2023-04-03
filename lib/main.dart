faa30fd20905c8efd425c7f4d55b989706fa52c6
ankushcode611 <ankush.code611@gmail.com>
2023-04-04 01:01:18 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_lifecycle_statefulwidget/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welsome to 30 Days of Flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
